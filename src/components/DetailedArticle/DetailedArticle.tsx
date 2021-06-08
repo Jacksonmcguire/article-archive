a5ef34f82bea42902a8c6f3261277aa0af0e899a
Jackson McGuire <jaxmcguire@gmail.com>
2021-06-08 14:03:17 -0400

    Begin building and styling details modal

--- src/components/DetailedArticle/DetailedArticle.tsx
+++ src/components/DetailedArticle/DetailedArticle.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './DetailedArticle.css';
+import { Story } from '../../utilities'
 /**
  * Modal or main component that is displayed when an article is clicked
  * responsible for invoking apiCall methods
  * @returns Modal with article info 
  */
-function DetailedArticle() {
+type DetailedProps = {
+  story?: Story
+}
+function DetailedArticle({story}: DetailedProps) {
+
   return (
-    <div className="App">
+    <div className="details">
+      {story?.abstract}
+      {story?.byline}
     </div>
   );
 }
