f83f67ede81b6531ba4366fd5c7d99cec6487ac6
Jackson McGuire <jaxmcguire@gmail.com>
2021-06-08 12:58:51 -0400

    Build basic functionality through app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { filterStories, getTopStories, Story } from '../../utilities';
+import { ArticleContainer } from '../ArticleContainer/ArticleContainer';
+import FeaturedArticle from '../FeaturedArticle/FeaturedArticle';
+import FilterForm from '../FilterForm/FilterForm';
 import './App.css';
 /**
  * Container for entire app
  * responsible for invoking apiCall methods
  * @returns A browser router that holds all components
  */
-function App() {
+
+ function App() {
+  const [currentStories, setCurrentStories] = useState([])
+  const [currentList, setCurrentList] = useState([])
+  const [featuredStory, setFeaturedStory] = useState() 
+  useEffect( () => {
+    if (!currentStories.length) {
+      getTopStories('books')
+      .then(data => {
+        setCurrentStories(data.results)
+        setCurrentList(data.results)
+      })
+    }
+  }, [currentStories])
+  
+  const filterByCategory = (cat: string): void => {
+    getTopStories(cat)
+    .then(data => {
+      setCurrentStories(data.results)
+      setCurrentList(data.results)
+    })
+  }
+
+  const filterBySearch = (term: string):void => {
+    setCurrentStories(currentList.filter(article => filterStories(article, term)))
+  }
+
   return (
     <div className="App">
+      <section className="top">
+        <FeaturedArticle></FeaturedArticle>
+        <FilterForm filterByCategory={filterByCategory} filterBySearch={filterBySearch}></FilterForm>
+        <h1>Article Archive</h1>
+      </section>
+      <ArticleContainer articles={currentStories}></ArticleContainer>
     </div>
   );
 }
