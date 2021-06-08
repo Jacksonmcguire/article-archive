1da332087b5de4bb285d59d111f0b32c216f9ad2
Jackson McGuire <jaxmcguire@gmail.com>
2021-06-08 18:08:11 -0400

    Restructure article card

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { SiNewyorktimes } from 'react-icons/si'
 import './Article.css';
 /**
  * Card component to show basic article information
@@ -15,14 +16,17 @@ type ArticleProps = {
 }
 function Article({byline, title, date, link}: ArticleProps) {
   return (
-    <Link to={title}>
-    <div className="article">
+    <div>
+    <a href={link} className="link"><SiNewyorktimes/></a>
+
+    <Link to={title} className="article">
       <h3>{title}</h3>
-      <p>{byline}</p>
-      <p>{date}</p>
-      <a href={link}>link</a>
-    </div>
+      <div className="card-info">
+        <p>{byline}</p>
+        <p>{date.split('T')[0].replaceAll('-', '/')}</p>
+      </div>
     </Link>
+    </div>
   );
 }
 
